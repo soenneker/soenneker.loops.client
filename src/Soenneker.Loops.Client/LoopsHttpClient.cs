c4040be904b13e7c1f74a7367a716d834a513afe
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:21 -0500

    xml commenting

--- src/Soenneker.Loops.Client/LoopsHttpClient.cs
+++ src/Soenneker.Loops.Client/LoopsHttpClient.cs
@@ -45,11 +45,18 @@ public sealed class LoopsHttpClient : ILoopsHttpClient
         }, cancellationToken);
     }
 
+    /// <summary>
+    /// Releases resources used by the current instance.
+    /// </summary>
     public void Dispose()
     {
         _httpClientCache.RemoveSync(_clientId);
     }
 
+    /// <summary>
+    /// Asynchronously releases resources used by the current instance.
+    /// </summary>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     public ValueTask DisposeAsync()
     {
         return _httpClientCache.Remove(_clientId);
